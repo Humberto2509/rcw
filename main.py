8791396d4d0ec2d0dda74ce241581cf85636a2f2
Humberto2509 <156677187+Humberto2509@users.noreply.github.com>
2024-07-05 16:36:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import uvicorn
+import subprocess
 
+
+app = FastAPI()
+
+
+origins=["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.on_event("startup")
+async def startup_event():
+    subprocess.Popen(["streamlit", "run", "appRF.py"])
+
+if __name__=='__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
